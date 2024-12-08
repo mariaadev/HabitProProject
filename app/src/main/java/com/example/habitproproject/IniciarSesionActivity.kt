e2a52d8cb29a90d6b6e6a77b9712b95df7ae7572
Maria Amzil <mini.mar@hotmail.com>
2024-12-08 13:49:26 +0100

    Añadiendo funcionalidad al botón de limpiar texto y al de mostrar contraseña

--- app/src/main/java/com/example/habitproproject/IniciarSesionActivity.kt
+++ app/src/main/java/com/example/habitproproject/IniciarSesionActivity.kt
@@ -1,20 +1,32 @@
 package com.example.habitproproject
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
 
 class IniciarSesionActivity : AppCompatActivity() {
+    private lateinit var passwordEditText: com.google.android.material.textfield.TextInputEditText
+    private lateinit var passwordLayout: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_iniciar_sesion)
+
+        passwordEditText = findViewById(R.id.etPassword)
+        passwordLayout = findViewById(R.id.textInputPasswordLayout)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+
 }
