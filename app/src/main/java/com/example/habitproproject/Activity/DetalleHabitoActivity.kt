775be8b0aa87c59742e6c3f909e0887f8e35b257
Maria Amzil <mini.mar@hotmail.com>
2025-01-21 19:56:08 +0100

    Añadida Activitat DetalleHabito

---
+++ app/src/main/java/com/example/habitproproject/Activity/DetalleHabitoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.habitproproject.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.habitproproject.R
+
+class DetalleHabitoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalle_habito)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
