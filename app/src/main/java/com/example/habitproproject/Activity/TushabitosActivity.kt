47d4e9362bc0684e8a55ba375db198f2b7f41412
Helena <helenagp05@gmail.com>
2025-01-26 13:26:59 +0100

    adapter de listas de habitos

---
+++ app/src/main/java/com/example/habitproproject/Activity/TushabitosActivity.kt
@@ -0,0 +1,46 @@
+package com.example.habitproproject.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.habitproproject.Adapter.HabitosAdapter
+import com.example.habitproproject.Adapter.TusHabitosAdapter
+import com.example.habitproproject.Model.Habitos
+import com.example.habitproproject.R
+
+class TushabitosActivity : AppCompatActivity() {
+
+    private lateinit var habitosAdapterMañana: TusHabitosAdapter
+    private lateinit var habitosAdapterTarde: TusHabitosAdapter
+    private lateinit var habitosAdapterNoche: TusHabitosAdapter
+    private lateinit var listaHabitosMañana: List<Habitos>
+    private lateinit var listaHabitosTarde: List<Habitos>
+    private lateinit var listaHabitosNoche: List<Habitos>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tushabitos)
+
+
+        listaHabitosMañana = listOf(
+            Habitos("Salir a correr", "30 min", 40, 30, "2023-12-02", "2024-02-01", false, R.drawable.ic_running),
+            Habitos("Beber agua", "3/8 vasos", 40, 30, "2023-12-02", "2024-02-01", false, R.drawable.ic_water)
+
+            )
+        listaHabitosTarde = listOf(
+            Habitos("Estudiar", "2 horas", 50, 120, "2023-12-01", "2024-01-01", false, R.drawable.ic_study),
+            Habitos("Programar", "3 horas", 20, 180, "2023-12-05", "2024-01-15", false, R.drawable.ic_coding)
+        )
+
+        listaHabitosNoche = listOf(
+            Habitos("Leer", "10 páginas", 40, 30, "2023-12-02", "2024-02-01", false, R.drawable.ic_read)
+        )
+
+        habitosAdapterMañana = TusHabitosAdapter(listaHabitosMañana)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/habitproproject/Adapter/TuHbitosAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.habitproproject.Adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.habitproproject.Activity.DetalleHabitoActivity
+import com.example.habitproproject.Adapter.HabitosAdapter.HabitosViewHolder
+import com.example.habitproproject.Model.Habitos
+import com.example.habitproproject.R
+
+class TusHabitosAdapter(
+    private val listaHabitos: List<Habitos>
+) : RecyclerView.Adapter<TusHabitosAdapter.HabitosViewHolderMañana>() {
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TusHabitosAdapter.HabitosViewHolderMañana {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list, parent, false)
+        return HabitosViewHolderMañana(view)
+    }
+
+    override fun getItemCount(): Int = listaHabitos.size
+
+    override fun onBindViewHolder(holder: TusHabitosAdapter.HabitosViewHolderMañana, position: Int) {
+        val habito = listaHabitos[position]
+        holder.bind(habito)
+
+        holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, DetalleHabitoActivity::class.java).apply {
+                putExtra("nombre", habito.nombre)
+                putExtra("duracion", habito.descripcion)
+                putExtra("progreso", habito.progreso)
+                putExtra("completado", habito.completado)
+                putExtra("fechaInicio", habito.fechaInicio)
+                putExtra("fechaFin", habito.fechaFin)
+                putExtra("imagenId", habito.imagenId)
+                putExtra("tiempoEnMinutos", habito.tiempoEnMinutos)
+
+            }
+            context.startActivity(intent)
+        }
+    }
+
+    class HabitosViewHolderMañana (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textNombreHabit: TextView = itemView.findViewById(R.id.textNombreHabit)
+        private val imageHabit: ImageView = itemView.findViewById(R.id.imageHabit)
+        fun bind(habito: Habitos) {
+            textNombreHabit.text = habito.nombre
+            imageHabit.setImageResource(habito.imagenId)
+        }
+
+    }
+}
