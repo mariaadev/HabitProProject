d98aac8b3d46497a7fc6c2c336273f553232f0c5
Maria Amzil <mini.mar@hotmail.com>
2025-02-27 17:28:31 +0100

    Añadidos @Post, @Put, @Delete a la interficie del ApiService

---
+++ app/src/main/java/com/example/habitproproject/API/ApiService.kt
@@ -0,0 +1,26 @@
+package com.example.habitproproject.API
+
+import com.example.habitproproject.Model.Habitos
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET("habitos")
+    fun getHabitos(): Call<List<Habitos>>
+
+    @POST("habitos")
+    fun createHabitos(@Body nuevoHabito: Habitos): Call<Habitos>
+
+    @PUT("habitos/{id}")
+    fun actualizarHabito(@Path("id") id: Int, @Body habito: Habitos): Call<Habitos>
+
+    @DELETE("habitos/{id}")
+    fun eliminarHabito(@Path("id") id: Int): Call<Unit>
+}
+
