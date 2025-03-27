867c342d2144ab75bdb2af9d3588afc48c837538
Maria Amzil <mini.mar@hotmail.com>
2025-03-27 17:26:35 +0100

    Añadidas dependencias para realizar estadisticas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.glide)
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
