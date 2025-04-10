1a79653910b8c73a8b771f30b54402059f17f64d
Helena <helenagp05@gmail.com>
2025-04-10 17:09:59 +0200

    creadas las classes ViewModel

---
+++ app/src/main/java/com/example/habitproproject/ViewModel/IniciarSesionViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.habitproproject.ViewModel
+
+class IniciarSesionViewModel {
+
+
+}
---
+++ app/src/main/java/com/example/habitproproject/ViewModel/RegistrarseViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.habitproproject.ViewModel
+
+class RegistrarseViewModel {
+
+}
